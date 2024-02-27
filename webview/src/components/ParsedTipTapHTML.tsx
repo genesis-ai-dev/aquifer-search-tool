a39ce7f09ebacedbdd10504b200d16f70d5b3273
Ben Scholtens <ben.scholtens@me.com>
2024-02-27 09:46:07 -0500

    start adding styles to tiptap

--- webview/src/components/ParsedTipTapHTML.tsx
+++ webview/src/components/ParsedTipTapHTML.tsx
@@ -1,16 +1,61 @@
 import React, { useMemo } from "react";
 
-import { Mark } from "@tiptap/core";
+import { Mark, Extension } from "@tiptap/core";
 // import { useEditor, EditorContent } from '@tiptap/react';
 import Highlight from "@tiptap/extension-highlight";
 import Link from "@tiptap/extension-link";
 import Image from "@tiptap/extension-image";
 import Subscript from "@tiptap/extension-subscript";
 import Superscript from "@tiptap/extension-superscript";
-import TextStyle from "@tiptap/extension-text-style";
+// import TextStyle from '@tiptap/extension-text-style';
 import Underline from "@tiptap/extension-underline";
 import StarterKit from "@tiptap/starter-kit";
 import { generateHTML } from "@tiptap/html";
+
+const CustomTextStyle = Extension.create({
+  name: "customTextStyle",
+
+  addGlobalAttributes() {
+    return [
+      {
+        types: ["textStyle"],
+        attributes: {
+          textColor: {
+            default: null,
+            renderHTML: (attributes) => {
+              if (!attributes.textColor) {
+                return {};
+              }
+              return { style: `color: ${attributes.textColor}` };
+            },
+          },
+          backgroundColor: {
+            default: null,
+            renderHTML: (attributes) => {
+              if (!attributes.backgroundColor) {
+                return {};
+              }
+              return {
+                style: `background-color: ${attributes.backgroundColor}`,
+              };
+            },
+          },
+        },
+      },
+      {
+        types: ["heading"],
+        attributes: {
+          paddingBottom: {
+            default: null,
+            renderHTML: () => {
+              return { style: `padding-bottom: 1rem` };
+            },
+          },
+        },
+      },
+    ];
+  },
+});
 
 const extensions = [
   StarterKit,
@@ -20,7 +65,7 @@ const extensions = [
   Highlight,
   Subscript,
   Superscript,
-  TextStyle,
+  CustomTextStyle, // Use your custom text style extension here
   Mark.create({ name: "bibleReference", renderHTML: () => ["span"] }),
   Mark.create({ name: "resourceReference", renderHTML: () => ["span"] }),
 ];
@@ -29,20 +74,6 @@ interface TipTapNode {
   type?: string;
   content?: TipTapNode[];
 }
-
-// function getAllUniqueTypesFromTipTapJSON(json: TipTapNode) {
-//   const types = new Set<string>();
-//   function traverse(obj: TipTapNode) {
-//     if ('type' in obj) {
-//       types.add(obj.type!);
-//     }
-//     if ('content' in obj && Array.isArray(obj.content)) {
-//       obj.content.forEach(traverse);
-//     }
-//   }
-//   traverse(json);
-//   return Array.from(types);
-// }
 
 function stripReferenceTypesFromTipTapJSON(
   json: TipTapNode,
@@ -67,10 +98,6 @@ interface ParsedTipTapHTMLProps {
 }
 
 const ParsedTipTapHTML: React.FC<ParsedTipTapHTMLProps> = ({ jsonContent }) => {
-  //   const editor = useEditor({
-  //     content: jsonContent?.content?.[0],
-  //     extensions,
-  //   });
   const output = useMemo(() => {
     if (jsonContent?.content?.[0]) {
       const strippedContent = stripReferenceTypesFromTipTapJSON(
@@ -87,30 +114,8 @@ const ParsedTipTapHTML: React.FC<ParsedTipTapHTMLProps> = ({ jsonContent }) => {
     return <div>No content</div>;
   }
 
-  //   let generatedHTML = '';
-  //   try {
-  //     generatedHTML = generateHTML(jsonContent?.content?.[0], extensions);
-  //   } catch (error) {
-  //     if (error instanceof Error) {
-  //       const uniqueTypes = getAllUniqueTypesFromTipTapJSON(
-  //         jsonContent?.content?.[0]['tiptap'] as TipTapNode,
-  //       );
-  //       const extensionNames = extensions
-  //         .map((ext) => 'name' in ext && ext.name)
-  //         .filter(Boolean);
-  //       generatedHTML = `<div>Error generating HTML: ${
-  //         error.message
-  //       }.<br/>Unique types in content: ${uniqueTypes.join(
-  //         ', ',
-  //       )}.<br/>Extensions used: ${extensionNames.join(', ')}.</div>`;
-  //     } else {
-  //       generatedHTML = `<div>An unknown error occurred.</div>`;
-  //     }
-  //   }
-
   return (
     <div>
-      {/* <EditorContent editor={editor} /> */}
       <div
         dangerouslySetInnerHTML={{
           __html: output,
