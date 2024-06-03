1de332bee289e797dbdfe737445de768ea76e8da
ryderwishart <ryderwishart@gmail.com>
2024-06-03 16:38:07 -0700

    chore: Update package.json and package-lock.json versions

--- webview/vite.config.ts
+++ webview/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
+      plugins: [nodePolyfills()],
       output: {
         entryFileNames: "assets/index.js",
         chunkFileNames: "assets/index.js",
