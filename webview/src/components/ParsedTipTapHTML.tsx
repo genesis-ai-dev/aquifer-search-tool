b4aed54039d014055aac32f03a91e54140f6daa5
Jake Wegner <jake.wegner@biblica.com>
2024-07-23 16:13:41 -0500

    Re-enable textStyle extension
    Solves this error when viewing the TBD article "Jesus Christ"
    
    ```
    index.js:40 RangeError: There is no mark type textStyle in this schema
        at kf.fromJSON (index.js:1582:8283)
        at h2.markFromJSON (index.js:1583:9369)
        at Array.map (<anonymous>)
        at Ef.fromJSON (index.js:1582:19966)
        at h2.nodeFromJSON (index.js:1583:9326)
        at Array.map (<anonymous>)
        at S.fromJSON (index.js:1582:6441)
        at Ef.fromJSON (index.js:1582:20114)
        at h2.nodeFromJSON (index.js:1583:9326)
        at Array.map (<anonymous>)
    ef @ index.js:40
    index.js:40 Uncaught RangeError: There is no mark type textStyle in this schema
        at kf.fromJSON (index.js:1582:8283)
        at h2.markFromJSON (index.js:1583:9369)
        at Array.map (<anonymous>)
        at Ef.fromJSON (index.js:1582:19966)
        at h2.nodeFromJSON (index.js:1583:9326)
        at Array.map (<anonymous>)
        at S.fromJSON (index.js:1582:6441)
        at Ef.fromJSON (index.js:1582:20114)
        at h2.nodeFromJSON (index.js:1583:9326)
        at Array.map (<anonymous>)
    ```

--- webview/src/components/ParsedTipTapHTML.tsx
+++ webview/src/components/ParsedTipTapHTML.tsx
@@ -7,7 +7,7 @@ import Link from "@tiptap/extension-link";
 import Image from "@tiptap/extension-image";
 import Subscript from "@tiptap/extension-subscript";
 import Superscript from "@tiptap/extension-superscript";
-// import TextStyle from '@tiptap/extension-text-style';
+import TextStyle from '@tiptap/extension-text-style';
 import Underline from "@tiptap/extension-underline";
 import StarterKit from "@tiptap/starter-kit";
 import { generateHTML } from "@tiptap/html";
@@ -95,6 +95,7 @@ const extensions = [
   Highlight,
   Subscript,
   Superscript,
+  TextStyle,
   CustomTextStyle, // Use your custom text style extension here
   CustomParagraph,
   CustomHeading,
